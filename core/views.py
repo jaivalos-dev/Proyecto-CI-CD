b2d99898d8a33f9740f52c1bc2762773a8467813
Javier Avalos <jaivalos.dev@gmail.com>
2025-10-16 13:21:51 -0600

    prueba

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Bienvenido esto es un cambio.")
+    return HttpResponse("Bienvenido esto es un cambio tra veeeeeeeeeeez.")
