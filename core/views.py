463fea2f8b913a69173d5b5b1edb8acd4e8de03e
Javier Avalos <jaivalos.dev@gmail.com>
2025-10-16 12:33:22 -0600

    Cambio en la vista

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Bienvenido al home de la aplicación Django.")
+    return HttpResponse("Bienvenido al home de la aplicación Django. Con un cambio en la aplicación.")
